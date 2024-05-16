d5eac9fefa65bac7064cdfc074e2cabc15befbd3
Валерий Печерский (м1) <pecherskiy.v@gmail.com>
2024-05-17 02:06:38 +0300

    popup

--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
 document.getElementById('openCoder').addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      var activeTab = tabs[0];
-      var repoUrl = activeTab.url;
-  
-      var coderUrl = 'https://you.domain/open?repo=' + encodeURIComponent(repoUrl);
-      chrome.tabs.create({ url: coderUrl });
+        var activeTab = tabs[0];
+        var repoUrl = activeTab.url;
+
+        // Загрузка сохраненных настроек
+        chrome.storage.sync.get(['coderUrl'], function(data) {
+            var coderUrl = data.coderUrl || 'https://your-coder-url.com/open?repo=';
+            var fullUrl = coderUrl + encodeURIComponent(repoUrl);
+            chrome.tabs.create({ url: fullUrl });
+        });
     });
-  });
-  
+});
